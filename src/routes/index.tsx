06568125011b839c23ce18ba92afcd049f284d3d
danilomartinss <danilolmartins@gmail.com>
2025-05-08 11:58:18 +0200

    Updated index.tsx
    - Created content for home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
+import { BookOpen, Fingerprint, Upload } from "lucide-react";
 
 export const Route = createFileRoute("/")({
 	component: Index,
@@ -6,10 +8,38 @@ export const Route = createFileRoute("/")({
 
 function Index() {
 	return (
-		<>
-			<div className="flex min-h-svh flex-col items-center justify-center">
-				Hello from Home!
+		<div className="flex flex-col items-center">
+			<div className="flex max-w-full flex-col gap-12 px-6">
+				<div className="flex flex-col items-start justify-center">
+					<h2 className="text-center lg:text-left">
+						Open Platform for Metadata Management and Tracking
+					</h2>
+					<h4 className="text-center font-normal lg:text-left">
+						For life scientists in Norway
+					</h4>
+				</div>
+
+				<div className="flex flex-col gap-8 sm:flex-row sm:flex-wrap sm:justify-center lg:justify-between">
+					<div className="flex flex-col items-center">
+						<BookOpen size={150} />
+						<Button className="mt-4 h-14 rounded-md px-6 has-[>svg]:px-4">
+							GET STARTED
+						</Button>
+					</div>
+					<div className="flex flex-col items-center">
+						<Upload size={150} />
+						<Button className="mt-4 h-14 rounded-md px-6 has-[>svg]:px-4">
+							UPLOAD DATA
+						</Button>
+					</div>
+					<div className="flex flex-col items-center">
+						<Fingerprint size={150} />
+						<Button className="mt-4 h-14 rounded-md px-6 has-[>svg]:px-4">
+							DATA ACCESS
+						</Button>
+					</div>
+				</div>
 			</div>
-		</>
+		</div>
 	);
 }
