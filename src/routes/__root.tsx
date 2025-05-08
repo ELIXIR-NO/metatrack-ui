830268efa0dc4cb184f718e3186e901bfd54a108
danilomartinss <danilolmartins@gmail.com>
2025-05-08 11:14:47 +0200

    Updated root.tsx
    - Changed padding on "main" tag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -24,7 +24,7 @@ export const Route = createRootRoute({
 						</div>
 					</header>
 					<hr />
-					<main className="mx-auto w-3/4 py-10 pt-40 pb-30">
+					<main className="mx-auto w-3/4 py-10 pt-50 pb-30">
 						<Outlet />
 					</main>
 					<Footer />
