ebddbc19fd97f98276d79340159e4abbda43a5c2
danilomartinss <danilolmartins@gmail.com>
2025-05-14 14:46:58 +0200

    Created card-grid.tsx
    - Added function to show card contents in a grid

---
+++ src/components/card-grid.tsx
@@ -0,0 +1,56 @@
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+export type CardGridData = {
+	title: string;
+	description: string;
+	image: string;
+	link: string;
+};
+
+export default function CardGrid({
+	data,
+	className = "",
+}: {
+	data: CardGridData[];
+	className?: string;
+}) {
+	return (
+		<div
+			className={cn(
+				"mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3",
+				className
+			)}
+		>
+			{data.map((item) => (
+				<Card
+					key={item.title}
+					className="flex h-full flex-col transition-shadow duration-300 hover:shadow-2xl"
+				>
+					<a href={item.link} className="flex h-full flex-col">
+						<CardHeader>
+							<CardTitle className="text-left text-lg">{item.title}</CardTitle>
+							<CardDescription></CardDescription>
+						</CardHeader>
+						<CardContent className="flex flex-col items-center justify-center space-y-6">
+							<img
+								src={item.image}
+								alt={item.title}
+								width={500}
+								height={250}
+								className="aspect-video self-center object-fill"
+							/>
+							<p className="text-justify text-sm">{item.description}</p>
+						</CardContent>
+					</a>
+				</Card>
+			))}
+		</div>
+	);
+}
