14dd7ef016092326c595812043bac88bf9c0faed
danilomartinss <danilolmartins@gmail.com>
2025-06-15 15:45:22 +0200

    Updated index.tsx
    - Updated content to show Resources and Stats Cards

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,11 @@
-import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
-import { BookOpen, Fingerprint, Upload } from "lucide-react";
-import CardGrid, { CardGridData } from "@/components/card-grid";
+import CardGrid from "@/components/cards/card-grid";
+import StatCardGrid from "@/components/cards/stat-card-grid";
+import type { CardGridData } from "@/components/cards/types";
+import { LogosModeToggle } from "@/components/logos-mode-toggle";
+import { statCards } from "@/lib/data/statCards";
+import { ThemeImage } from "@/components/theme-image";
+import QuickStart from "@/components/quick-start";
 
 export const Route = createFileRoute("/")({
 	component: Index,
@@ -11,25 +15,37 @@ const resources: CardGridData[] = [
 	{
 		title: "Pathogen Platform Norway",
 		description: "",
-		link: "/",
-		image: "/",
+		link: "https://www.pathogens.no",
+		image: (
+			<ThemeImage
+				lightSrc={"/pathogens_portal_norway_logo_dark.png"}
+				darkSrc={"/pathogens_portal_norway_logo_light.png"}
+				alt={"Pathogen Logo"}
+			/>
+		),
 	},
 	{
-		title: "Other Page",
+		title: "Elixir Page",
 		description: "",
-		link: "/",
-		image: "",
+		link: "https://www.elixir.no",
+		image: (
+			<LogosModeToggle
+				className="aspect-video object-contain hover:ring-0"
+				width={400}
+				height={250}
+			/>
+		),
 	},
 ];
 
 function Index() {
 	return (
 		<div className="flex flex-col items-center space-y-12 text-justify">
-			<div className="flex max-w-full flex-col gap-12">
+			<div className="flex flex-col gap-12">
 				<div className="flex flex-col">
-					<h2 className="text-justify lg:text-left">
+					<h1 className="text-bold text-justify lg:text-left">
 						Open Platform for Metadata Management and Tracking
-					</h2>
+					</h1>
 					<h4 className="text-justify font-normal lg:text-left">
 						For life scientists in Norway
 					</h4>
@@ -38,34 +54,12 @@ function Index() {
 					</a>
 				</div>
 
-				<div className="flex flex-col gap-8 sm:flex-row sm:flex-wrap sm:justify-center lg:justify-between">
-					<div className="flex flex-col items-center">
-						<BookOpen size={150} />
-						<Button className="mt-4 h-14 rounded-md px-6 has-[>svg]:px-4">
-							GET STARTED
-						</Button>
-					</div>
-					<div className="flex flex-col items-center">
-						<Upload size={150} />
-						<Button className="mt-4 h-14 rounded-md px-6 has-[>svg]:px-4">
-							UPLOAD DATA
-						</Button>
-					</div>
-					<div className="flex flex-col items-center">
-						<Fingerprint size={150} />
-						<Button className="mt-4 h-14 rounded-md px-6 has-[>svg]:px-4">
-							DATA ACCESS
-						</Button>
-					</div>
+				<QuickStart />
+				<div className="w-full rounded-md bg-gray-100 p-6">
+					<h2 className="text-center text-black lg:text-left">Resources</h2>
+					<CardGrid data={resources} className="lg:grid-cols-2" />
 				</div>
-			</div>
-			<div className="rounded-md bg-gray-100 p-6">
-				<h2 className="text-center lg:text-left">Resources</h2>
-				<CardGrid data={resources} className="lg:grid-cols-2" />
-			</div>
-			<div className="rounded-md bg-gray-100 p-6">
-				<h2 className="text-center lg:text-left">Numbers Behind MetaTrack</h2>
-				<CardGrid data={resources} className="lg:grid-cols-2" />
+				<StatCardGrid data={statCards} />
 			</div>
 		</div>
 	);
