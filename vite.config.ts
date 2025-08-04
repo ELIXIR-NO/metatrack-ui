e1173288bf138adb3b511b5a0ca3d035430bcec2
danilomartinss <danilolmartins@gmail.com>
2025-08-04 21:45:05 +0200

    Updated vite.config.ts
    - Added server config for api

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,22 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [
+		TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+		react(),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:3000",
+				changeOrigin: true,
+			},
+		},
+	},
 });
