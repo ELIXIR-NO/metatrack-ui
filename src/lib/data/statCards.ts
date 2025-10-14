a83627def363864fa731c8e7df1b237a0aefc4bd
danilomartinss <danilolmartins@gmail.com>
2025-10-14 15:00:15 +0200

    chore: updated stats to show in home page

--- src/lib/data/statCards.ts
+++ src/lib/data/statCards.ts
@@ -1,30 +1,28 @@
 import type { StatCardData } from "@/components/cards/types";
+import { BsClipboard2Data } from "react-icons/bs";
+import { RiDatabaseLine } from "react-icons/ri";
+import { MdDateRange } from "react-icons/md";
 
 export const statCards: StatCardData[] = [
 	{
 		type: "text",
-		title: "CONTRIBUTORS",
-		numberText: 300,
-		description: "Data uploaders",
+		title: "",
+		numberText: 1991,
+		description: "Samples",
+		icon: BsClipboard2Data,
 	},
 	{
-		type: "chart",
-		title: "TYPE OF MICROBIAL SPECIES",
-		unit: "species",
-		data: [
-			{ label: "Bacteria", value: 529, fill: "var(--chart-1)" },
-			{ label: "Fungi", value: 197, fill: "var(--chart-2)" },
-			{ label: "Archaea", value: 102, fill: "var(--chart-3)" },
-			{ label: "Viruses", value: 65, fill: "var(--chart-4)" },
-		],
+		type: "text",
+		title: "",
+		numberText: 20,
+		description: "Projects",
+		icon: RiDatabaseLine,
 	},
 	{
-		type: "chart",
-		title: "TYPE OF PLATFORMS",
-		unit: "Sequencing",
-		data: [
-			{ label: "PacBio", value: 40.8, fill: "var(--chart-1)" },
-			{ label: "ONT", value: 59.2, fill: "var(--chart-2)" },
-		],
+		type: "date",
+		title: "",
+		dateText: "10/14/2025",
+		description: "Last submission date",
+		icon: MdDateRange,
 	},
 ];
