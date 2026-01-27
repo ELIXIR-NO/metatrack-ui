295ca968a541fca2adae47780a3965d832e5f701
danilomartinss <danilolmartins@gmail.com>
2026-01-27 13:35:02 +0100

    Added more global variables for sample tables

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,17 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import "@tanstack/react-table";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-export const NON_EDITABLE_COLUMNS = ["sample_alias"];
+export const QUICK_EDIT_LIMIT = 3;
+export const NON_EDITABLE_COLUMNS = ["alias"];
+export const NON_VIEWED_COLUMNS = ["id", "createdOn", "lastUpdatedOn"];
+
+declare module "@tanstack/react-table" {
+	interface ColumnMeta<TData extends unknown, TValue> {
+		label?: string;
+	}
+}
