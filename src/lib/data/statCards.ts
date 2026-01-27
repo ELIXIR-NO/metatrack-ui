409bec2e892e201bfd26d25b7a01b919b2dd3442
danilomartinss <danilolmartins@gmail.com>
2026-01-27 13:35:37 +0100

    Updated statistics to get from api

--- src/lib/data/statCards.ts
+++ src/lib/data/statCards.ts
@@ -1,28 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
 import type { StatCardData } from "@/components/cards/types";
 import { BsClipboard2Data } from "react-icons/bs";
 import { RiDatabaseLine } from "react-icons/ri";
 import { MdDateRange } from "react-icons/md";
+import { getProjectsPublic } from "../api-keycloak";
 
-export const statCards: StatCardData[] = [
-	{
-		type: "text",
-		title: "",
-		numberText: 1991,
-		description: "Samples",
-		icon: BsClipboard2Data,
-	},
-	{
-		type: "text",
-		title: "",
-		numberText: 20,
-		description: "Projects",
-		icon: RiDatabaseLine,
-	},
-	{
-		type: "date",
-		title: "",
-		dateText: "10/14/2025",
-		description: "Last submission date",
-		icon: MdDateRange,
-	},
-];
+export function useDashboardStats() {
+	return useQuery({
+		queryKey: ["dashboard-stats"],
+		queryFn: async (): Promise<StatCardData[]> => {
+			const projects = await getProjectsPublic();
+
+			return [
+				{
+					type: "text",
+					title: "",
+					numberText: 1994,
+					description: "Samples",
+					icon: BsClipboard2Data,
+				},
+				{
+					type: "text",
+					title: "",
+					numberText: projects.length,
+					description: "Projects",
+					icon: RiDatabaseLine,
+				},
+				{
+					type: "date",
+					title: "",
+					dateText: "01/14/2026",
+					description: "Last submission date",
+					icon: MdDateRange,
+				},
+			];
+		},
+	});
+}
