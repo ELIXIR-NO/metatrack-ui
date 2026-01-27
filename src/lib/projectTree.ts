32ab2fd012b33cb9f653f23b8c6fc7731f7c0eff
danilomartinss <danilolmartins@gmail.com>
2026-01-27 13:34:17 +0100

    Removed study and assay

--- src/lib/projectTree.ts
+++ src/lib/projectTree.ts
@@ -1,23 +1,10 @@
-import { Assay, Project, Sample, Study } from "./types";
+import { Project, Sample } from "./types";
 
-export function buildProjectTree(
-	project: Project,
-	studies: Study[],
-	assays: Assay[],
-	samples: Sample[]
-) {
+export function buildProjectTree(project: Project, samples: Sample[]) {
 	return {
-		name: project.title,
-		children: studies.map((study) => ({
-			name: study.title,
-			children: assays
-				//.filter((a) => a.filename === study.id)
-				.map((assay) => ({
-					name: assay.filename,
-					children: samples
-						.filter((s) => s.id !== assay.id)
-						.map((sample) => ({ name: sample.name })),
-				})),
+		name: project.name,
+		children: samples.map((sample) => ({
+			name: sample.alias,
 		})),
 	};
 }
