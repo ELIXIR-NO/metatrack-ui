dcfaa1bb14a47d6b30b043fa62350421fceb9649
danilomartinss <danilolmartins@gmail.com>
2026-05-28 12:20:04 +0200

    chore: fixed tab content size

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -12,45 +12,45 @@ function About() {
 	return (
 		<Suspense>
 			<Tabs defaultValue="what">
-				<TabsList variant="underline">
-					<TabsTrigger value="what">
+				<TabsList variant="line"  className="flex justify-center w-full">
+					<TabsTrigger value="what" className="hover:text-foreground text-lg font-semibold text-gray-500">
 						What is MetaTrack
 					</TabsTrigger>
 
-					<TabsTrigger value="people">
+					<TabsTrigger value="people" className="hover:text-foreground text-lg font-semibold text-gray-500">
 						Why MetaTrack
 					</TabsTrigger>
 
-					<TabsTrigger value="partners">
+					<TabsTrigger value="partners" className="hover:text-foreground text-lg font-semibold text-gray-500">
 						Who is MetaTrack for
 					</TabsTrigger>
 
-					<TabsTrigger value="projects">
+					<TabsTrigger value="projects" className="hover:text-foreground text-lg font-semibold text-gray-500">
 						Behind MetaTrack
 					</TabsTrigger>
 
-					<TabsTrigger value="contact-us">
+					<TabsTrigger value="contact-us" className="hover:text-foreground text-lg font-semibold text-gray-500">
 						Funding MetaTrack
 					</TabsTrigger>
 				</TabsList>
-
-				<TabsContent value="what">
+				
+				<TabsContent value="what" className="min-h-[70vh]">
 					<What />
 				</TabsContent>
 
-				<TabsContent value="people">
+				<TabsContent value="people" className="min-h-[70vh]">
 					<Why />
 				</TabsContent>
 
-				<TabsContent value="partners">
+				<TabsContent value="partners" className="min-h-[70vh]">
 					<Who />
 				</TabsContent>
 
-				<TabsContent value="projects">
+				<TabsContent value="projects" className="min-h-[70vh]">
 					<Behind />
 				</TabsContent>
 
-				<TabsContent value="contact-us">
+				<TabsContent value="contact-us" className="min-h-[70vh]">
 					<Funding />
 				</TabsContent>
 			</Tabs>
