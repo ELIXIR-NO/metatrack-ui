6a0db4cdb96ee99ecebeef63012dcdf535fd0ce3
danilomartinss <danilolmartins@gmail.com>
2026-05-29 14:41:49 +0200

    chore: Removed Overview label

--- src/components/dashboard/site-header.tsx
+++ src/components/dashboard/site-header.tsx
@@ -8,9 +8,7 @@ import {
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import {
-  Fragment
-} from "react";
+import { Fragment } from "react";
 
 interface BreadcrumbItemProps {
 	label: string;
@@ -22,10 +20,7 @@ interface SiteHeaderProps {
 }
 
 export function SiteHeader({ items = [] }: SiteHeaderProps) {
-	const DefaultItems: BreadcrumbItemProps[] = [
-		{ label: "Overview", href: "/dashboard" },
-		...items,
-	];
+	const DefaultItems: BreadcrumbItemProps[] = [...items];
 	return (
 		<header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
 			<div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
