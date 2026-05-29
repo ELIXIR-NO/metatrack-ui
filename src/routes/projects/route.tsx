90f526ebbf29bba7d61f4195427974bfd342f05b
danilomartinss <danilolmartins@gmail.com>
2026-05-29 14:57:24 +0200

    chore: Removed dashboard path

---
+++ src/routes/projects/route.tsx
@@ -0,0 +1,48 @@
+import { createFileRoute, Outlet, useNavigate } from "@tanstack/react-router";
+import { AppSidebar } from "@/components/dashboard/app-sidebar";
+import { SidebarSkeleton } from "@/components/sidebar-skeleton";
+import { useUser } from "@/hooks/use-user";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+import { useEffect } from "react";
+
+export const Route = createFileRoute("/projects")({
+	component: DashboardLayout,
+});
+
+function DashboardLayout() {
+	const { data: user, isLoading } = useUser();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!isLoading && !user) {
+			navigate({ to: "/" });
+		}
+	}, [user, isLoading, navigate]);
+
+	if (isLoading) {
+		return (
+			<SidebarProvider>
+				<SidebarSkeleton />
+			</SidebarProvider>
+		);
+	}
+
+	if (!user) return null;
+
+	return (
+		<SidebarProvider
+			className="min-h-full"
+			style={
+				{
+					"--sidebar-width": "14rem",
+				} as React.CSSProperties
+			}
+		>
+			<AppSidebar variant="inset" user={user} />
+			<SidebarInset>
+				<Outlet />
+			</SidebarInset>
+		</SidebarProvider>
+	);
+}
