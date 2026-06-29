6715beb6d45479a2a74895c0b7e81692dc14d52a
Ralph Niederlöhner <ralphn@uio.no>
2026-06-29 16:22:59 +0200

    fix: removed dev setting

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,10 +1,10 @@
 export const API_URL = import.meta.env.PROD
 	? "https://api.metatrack.no/api"
-	: "http://localhost:1234/api";
+	: "/api";
 
 export const API_BASE_URL = import.meta.env.PROD
 	? "https://api.metatrack.no"
-	: "http://localhost:1234";
+	: "";
 
 export const KEYCLOAK_URL = import.meta.env.PROD
 	? "https://auth.metatrack.no"
